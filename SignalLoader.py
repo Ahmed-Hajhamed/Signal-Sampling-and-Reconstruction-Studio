3ef9b1af55b6b8ea179b4525a30e6ff1aac223c4
ah03ad63 <125690012+ah03ad63@users.noreply.github.com>
2024-10-25 10:53:04 +0300

    Create SignalLoader.py

---
+++ SignalLoader.py
@@ -0,0 +1,16 @@
+class SignalLoader:
+    def load_signal_from_file(self, filepath):
+        # Load the signal from the specified file
+        pass
+
+    def load_signal_from_mixer(self, signal):
+        # Load a user-composed signal
+        pass
+
+    def add_noise(self, signal, snr):
+        # Add noise to the signal based on SNR
+        pass
+
+    def get_loaded_signal(self):
+        # Return the currently loaded signal
+        pass
