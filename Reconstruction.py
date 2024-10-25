410516ad662ea10bb9634ea9d1ada1b3cf588044
ah03ad63 <125690012+ah03ad63@users.noreply.github.com>
2024-10-25 10:53:01 +0300

    Create Reconstruction.py

---
+++ Reconstruction.py
@@ -0,0 +1,10 @@
+class Reconstruction:
+    @staticmethod
+    def whittaker_shannon(sampled_signal, sampling_frequency):
+        # Perform Whittaker-Shannon reconstruction
+        pass
+
+    @staticmethod
+    def other_methods(sampled_signal):
+        # Implement other reconstruction methods
+        pass
