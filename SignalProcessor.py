7c516814c451d407d84e73c929ca215f3a1ed5d5
ah03ad63 <125690012+ah03ad63@users.noreply.github.com>
2024-10-25 23:00:29 +0300

    Update SignalProcessor.py

--- SignalProcessor.py
+++ SignalProcessor.py
@@ -5,10 +5,12 @@ class SignalProcessor:
 
     def recover_signal(self, sampled_signal, method):
         # Reconstruct the signal using the specified method
+        # Outputs a 2D numpy array
         pass
 
     def calculate_difference(self, original_signal, recovered_signal):
         # Calculate the difference between original and recovered signals
+        # Outputs signals_difference in a 2D numpy array
         pass
 
     def fourier_transform(self, signal):
