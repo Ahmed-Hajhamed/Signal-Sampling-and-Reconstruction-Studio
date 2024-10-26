9526c6273096c2ae650ffc2049938b14dcf33246
zeyad-wail <zeyadalshakhy@gmail.com>
2024-10-26 18:16:20 +0300

    change the files of SignalLoader.py and SignalMixer.py make them as module
    SignalMixer.py :
    add composed_signal and time variables
    add set_time and create_signal functions

--- SignalMixer.py
+++ SignalMixer.py
@@ -1,19 +1,47 @@
 import numpy as np
 
+components = []
+composed_signal = None
+time = 0
 
-class SignalMixer:
-    components = []
 
-    def add_sinusoidal_component(self, frequency, amplitude, duration):
-        # Add a sinusoidal component to the signal
+def create_signal(frequency, amplitude):
+    global time
+    signal = amplitude * np.cos(2 * np.pi * frequency * time)
+    return signal
 
-        SignalMixer.components.append(amplitude * np.cos(2 * np.pi * frequency * duration))
-        pass
 
-    def remove_component(self, index):
-        # Remove a sinusoidal component
-        SignalMixer.components.pop(index)
+def set_time(duration):
+    global time
+    sampling =100
+    time = np.linspace(0, duration, int(sampling * duration))
 
-    def get_composed_signal(self):
-        # Return the composed signal
-        return SignalMixer.components
+
+def add_sinusoidal_component(frequency, amplitude):
+    # Add a sinusoidal component to the signal
+    global components
+    global composed_signal
+
+    components.append((frequency, amplitude))
+    signal = create_signal(frequency, amplitude)
+    if composed_signal:
+        composed_signal = composed_signal + signal
+    else:
+        composed_signal = signal
+
+
+def remove_component(index):
+    # Remove a sinusoidal component
+    global components
+    global composed_signal
+
+    signal_component = components.pop(index)
+    frequency, amplitude = signal_component[0], signal_component[1]
+    signal = create_signal(frequency, amplitude)
+    if composed_signal:
+        composed_signal = composed_signal - signal
+
+
+def get_composed_signal():
+    # Return the composed signal
+    return composed_signal
