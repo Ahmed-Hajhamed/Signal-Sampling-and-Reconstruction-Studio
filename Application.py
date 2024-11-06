253d94e60f454a635692dcbe3d2305eaaae842d2
zeyad-wail <zeyadalshakhy@gmail.com>
2024-11-06 11:00:36 +0200

    Ahmed don't touch

--- Application.py
+++ Application.py
@@ -2,9 +2,8 @@ from ui import SamplingTheoryStudio
 import sys
 from PyQt5.QtWidgets import QApplication
 from qt_material import apply_stylesheet
-class Application:
-    def __init__(self):
-        super().__init__()
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = SamplingTheoryStudio()
