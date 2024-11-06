bc2645061b6b759f8aafaf73c7a8347a6ac24a6c
zeyad-wail <zeyadalshakhy@gmail.com>
2024-11-06 11:03:53 +0200

    test of github

--- Application.py
+++ Application.py
@@ -3,6 +3,7 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from qt_material import apply_stylesheet
 
+# test of github to fix the problem
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
